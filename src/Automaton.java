ff72293c1eaaa6ba4fb6242bfbd4341ddee6e850
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2019-12-31 18:05:11 +0200

    Refactored file read and rearranged methods + small changes in Automaton class

--- src/Automaton.java
+++ src/Automaton.java
@@ -10,25 +10,49 @@ public abstract class Automaton {
 	static ArrayList<State> states = new ArrayList<State>();
 	static Set<State> currentState = new HashSet<>();
 
-	public static void createAutomatonFromFile(String filePath) {
-		String json = new String();
-		boolean fileError = false;
-		
-		//file read
-		try {
-			Scanner sc = new Scanner(new File(filePath));
-			while(sc.hasNextLine()) {
-				json += sc.nextLine();
-			}
-			sc.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found. Check path and try again.");
-			fileError = true;
-		}
+    public static ArrayList<State> getStates() {
+        return states;
+    }
 
-		if (!fileError) {
+    public static Set<State> getCurrentState() {
+        return currentState;
+    }
+
+    public static void setCurrentState(Set<State> currentState) {
+        Automaton.currentState = currentState;
+    }
+
+    public static void addState(State s) {
+        getStates().add(s);
+    }
+
+    public static void printAutomatonDetails() {
+        for(State s : getStates()) {
+            s.printStateDetails();
+        }
+    }
+
+    private static String readFile(String filePath) {
+        String json = new String();
+        try {
+            Scanner sc = new Scanner(new File(filePath));
+            while(sc.hasNextLine()) {
+                json += sc.nextLine();
+            }
+            sc.close();
+            return json;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Check path and try again.");
+            return null;
+        }
+    }
+
+	public static void createAutomatonFromFile(String filePath) {
+		String json = readFile(filePath);
+
+        if (json != null) {
+            //import JSON data to variable "states"
 			try {
-				//import JSON data to controller variable "states"
 				getStates().clear();
 				JSONObject obj = new JSONObject(json);
 				int numOfStates = obj.getInt("states");
@@ -53,51 +77,10 @@ public abstract class Automaton {
 				System.out.println("Automaton loaded from path \"" + filePath + "\". To see the automaton's details, choose option 2=Print automaton details.");
 			} catch (JSONException e) {
 				System.out.println("File format is not correct. Make sure it is a JSON file and the format follows the instructions. Check file and try again.");
-				createAutomatonFromFile("files/automaton.json"); //resets any changes that where possibly made in the automaton before wrong format error
+				//resets any changes that where possibly made in the automaton before wrong format error by loading default file
+				createAutomatonFromFile("files/automaton.json");
 			}
 		}
-	}
-	
-	public static void checkWord(String w) {
-		getCurrentState().clear();
-		//find start state
-		for (State s : getStates()) {
-			if(s.isStart()) {
-				getCurrentState().add(s);
-				break;
-			}
-		}
-
-		boolean transitionPossible = true; //if current state has no possible transitions and the word reading is not completed, this flag becomes false
-		for(char c : w.toCharArray()) {
-			//Debugging
-//			System.out.println("Symbol: " + c);
-//			System.out.println("Current states: " + getCurrentState().toString());
-
-//			transitionPossible = false;
-//			for (State s : getCurrentState()) {
-//				//Debugging
-////				System.out.println("Possible transitions: " + s.getTransitions().toString());
-//				for(Transition t : s.getTransitions()) {
-//					if (t.getSymbol() == c) {
-//						getNextState().add(t.getOutState());
-//						transitionPossible = true;
-//					}
-//				}
-//			}
-
-			Set<State> temp = findNextState(getCurrentState(), c); //temporary set of state objects to store the next possible states
-			transitionPossible = !temp.isEmpty();
-
-			if (transitionPossible){
-				//Sets the newly created set to the current one
-				setCurrentState(temp);
-			} else {
-				break;
-			}
-		}
-		
-		System.out.println("Word " + w + " is "+ (currentStateFinal() && transitionPossible?"":"not ") + "part of the automaton");
 	}
 
 	public static boolean currentStateFinal(){
@@ -105,22 +88,6 @@ public abstract class Automaton {
 			if (s.isFinal()) return true;
 		}
 		return false;
-	}
-	
-	public static ArrayList<State> getStates() {
-		return states;
-	}
-
-	public static Set<State> getCurrentState() {
-		return currentState;
-	}
-
-	public static void setCurrentState(Set<State> currentState) {
-		Automaton.currentState = currentState;
-	}
-
-	public static void addState(State s) {
-		getStates().add(s);
 	}
 
 	public static Set<State> findNextState(Set<State> currentState, char c){
@@ -136,9 +103,45 @@ public abstract class Automaton {
 		return nextState;
 	}
 
-	public static void printAutomatonDetails() {
-		for(State s : getStates()) {
-			s.printStateDetails();
-		}
-	}
+    public static void checkWord(String w) {
+        getCurrentState().clear();
+        //find start state
+        for (State s : getStates()) {
+            if(s.isStart()) {
+                getCurrentState().add(s);
+                break;
+            }
+        }
+
+        boolean transitionPossible = true; //if current state has no possible transitions and the word reading is not completed, this flag becomes false
+        for(char c : w.toCharArray()) {
+            //Debugging
+//			System.out.println("Symbol: " + c);
+//			System.out.println("Current states: " + getCurrentState().toString());
+
+//			transitionPossible = false;
+//			for (State s : getCurrentState()) {
+//				//Debugging
+////				System.out.println("Possible transitions: " + s.getTransitions().toString());
+//				for(Transition t : s.getTransitions()) {
+//					if (t.getSymbol() == c) {
+//						getNextState().add(t.getOutState());
+//						transitionPossible = true;
+//					}
+//				}
+//			}
+
+            Set<State> temp = findNextState(getCurrentState(), c); //temporary set of state objects to store the next possible states
+            transitionPossible = !temp.isEmpty();
+
+            if (transitionPossible){
+                //Sets the newly created set to the current one
+                setCurrentState(temp);
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("Word " + w + " is "+ (currentStateFinal() && transitionPossible?"":"not ") + "part of the automaton");
+    }
 }
